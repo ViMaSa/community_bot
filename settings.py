4f2c1f6f160466b60eeba767c0d727f70207dc2b
iShabang <curtin.shannon@gmail.com>
2021-01-26 00:14:27 -0800

    updating settings to be an object

--- settings.py
+++ settings.py
@@ -2,14 +2,11 @@ from dotenv import load_dotenv
 import os
 
 #load: loads the variables from '.env' and stores them into memory
-def load():
-    load_dotenv()
-    global DEVELOPER_KEY
-    global PLAYLIST_ID
-    global MAX_RESULTS
-    global DISCORD_TOKEN
 
-    DEVELOPER_KEY = os.getenv('DEVELOPER_KEY')
-    PLAYLIST_ID = os.getenv('PLAYLIST_ID')
-    MAX_RESULTS = int(os.getenv('MAX_RESULTS'))
-    DISCORD_TOKEN = os.getenv('DT')
+class Settings(object):
+    def __init__(self):
+        load_dotenv()
+        self.GOOGLE_DEVELOPER_KEY = os.getenv('GOOGLE_DEVELOPER_KEY')
+        self.PLAYLIST_ID = os.getenv('PLAYLIST_ID')
+        self.MAX_RESULTS = int(os.getenv('MAX_RESULTS'))
+        self.DISCORD_TOKEN = os.getenv('DISCORD_AUTH_TOKEN')
